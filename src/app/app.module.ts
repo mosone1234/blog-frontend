586a1938dd9064c99ea268466912d9ced2cae08e
mos <xhosone@gmail.com>
2019-05-05 02:21:51 -0400

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
+import { UserComponent } from './components/admin/user/user.component';
+import { ArticleComponent } from './components/admin/article/article.component';
+import { CommentComponent } from './components/admin/comment/comment.component';
+import { DashboardComponent } from './components/user/dashboard/dashboard.component';
+import { NavbarComponent } from './components/user/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    UserComponent,
+    ArticleComponent,
+    CommentComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
